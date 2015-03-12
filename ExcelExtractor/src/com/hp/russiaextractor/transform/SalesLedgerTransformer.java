1a5bfe1b7ebf9443f9cfcbfc1977b6da23ae5381
Antonio Gage <antoniojgage@gmail.com>
2015-03-12 11:01:28 -0500

    Changes without VO

--- ExcelExtractor/src/com/hp/russiaextractor/transform/SalesLedgerTransformer.java
+++ ExcelExtractor/src/com/hp/russiaextractor/transform/SalesLedgerTransformer.java
@@ -8,6 +8,7 @@ import java.io.InputStream;
 import java.util.ArrayList;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -17,16 +18,24 @@ import com.hp.russiaextractor.vo.SalesLedgerVO;
 
 public class SalesLedgerTransformer {
 
-	public void readWriteSpreadsheet(File in, File out) throws FileNotFoundException {
-		InputStream inputS = new FileInputStream(in);
-		ArrayList<SalesLedgerVO> salesLedgerList = new ArrayList<SalesLedgerVO>();
+	public static void main(String[] args) throws FileNotFoundException {
+	// public void readWriteSpreadsheet(File in, File out) throws FileNotFoundException {
+		
+		String in2 = "C:\\Users\\gagean\\Documents\\FY15-GFIT-GRC-FIN-STD-BRAZIL\\Russia Java Project\\Sales Ledger HPFS Analysis.xlsx";
+		
+		InputStream inputS = new FileInputStream(in2);
+		// ArrayList<SalesLedgerVO> salesLedgerList = new ArrayList<SalesLedgerVO>();
 		
 		try {
 			Workbook wb = WorkbookFactory.create(inputS);
 			Sheet sheet = wb.getSheetAt(0);
-			boolean getNextRow = true;
-			Row row = null;
-			for (int r = 0; getNextRow; row = sheet.getRow(r++)) {
+			// boolean getNextRow = true;
+			//Row row = null;
+			Row row = sheet.getRow(23);
+			Cell cell = row.getCell(27);
+			System.out.print(cell.getStringCellValue());
+
+			/* for (int r = 0; getNextRow; row = sheet.getRow(r++)) {
 				SalesLedgerVO salesLedgerVO = new SalesLedgerVO();
 				//Field No
 				salesLedgerVO.setNo(new Double(row.getCell(0).getNumericCellValue()).intValue());
@@ -37,7 +46,8 @@ public class SalesLedgerTransformer {
 				
 				
 				
-			}
+			} */
+			
 		} catch (InvalidFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
